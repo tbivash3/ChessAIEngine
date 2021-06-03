491359e2f818871aa22eb3b830b1ed1e8234a9e1
Bivash Thakuri <tbivash3@gmail.com>
2021-06-03 00:21:36 -0500

    Create Chess Board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+
+  COLOR_WHITE = 'white';
+
+  COLOR_BLACK = 'black';
+
   title = 'ChessAIEngine';
+
+  boxSize = 0;
+
+  numOfBoxes: number[] = [];
+
+  @ViewChild('board')
+  board!: ElementRef;
+
+  ngOnInit(): void {
+    this.numOfBoxes = Array.from(Array(64).keys())
+  }
+
+  ngAfterViewInit(): void {
+    this.boxSize = 100;
+  }
+
+  getBackgroundColor(index: number) {
+
+    let color = 'white';
+
+    const row = Math.floor(index / 8);
+    const column = index % 8;
+
+    if ((row + column) % 2 === 0) {
+      color = 'black';
+    }
+
+    return color;
+
+  }
+
 }
